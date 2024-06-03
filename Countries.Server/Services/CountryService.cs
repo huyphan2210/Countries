581ad699de0ad73d233c819b8ab2eb35b4b6ca55
Huy Phan <huy.phan2210@outlook.com>
2024-06-03 13:49:45 +0700

    Create CountryService

---
+++ Countries.Server/Services/CountryService.cs
@@ -0,0 +1,25 @@
+﻿using Countries.Server.Models;
+using Countries.Server.Repositories;
+
+namespace Countries.Server.Services
+{
+    public class CountryService : ICountryService
+    {
+        private readonly ICountryRepository _countryRepository;
+
+        public CountryService(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+
+        public async Task<IList<Country>> GetCountries()
+        {
+            return await _countryRepository.GetCountries();
+        }
+
+        public async Task<IList<Country>> GetCountriesWithFilter()
+        {
+            return await _countryRepository.GetCountriesWithFilter();
+        }
+    }
+}
---
+++ Countries.Server/Services/ICountryService.cs
@@ -0,0 +1,10 @@
+﻿using Countries.Server.Models;
+
+namespace Countries.Server.Services
+{
+    public interface ICountryService
+    {
+        public Task<IList<Country>> GetCountries();
+        public Task<IList<Country>> GetCountriesWithFilter();
+    }
+}
