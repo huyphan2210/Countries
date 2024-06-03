55b3e9073ae70c8556cab1aa0956a03476917e54
Huy Phan <huy.phan2210@outlook.com>
2024-06-03 13:49:35 +0700

    Create Country Repository
    Fetch data from Database

---
+++ Countries.Server/Repositories/CountryRepository.cs
@@ -0,0 +1,44 @@
+﻿using Countries.Server.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Countries.Server.Repositories
+{
+
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly IMongoCollection<Country> _countries;
+
+        public CountryRepository(IConfiguration configuration)
+        {
+            var connectionStringTemplate = configuration.GetConnectionString("MongoDB") ?? "";
+            var connectionPassword = Environment.GetEnvironmentVariable("MONGODB_PASSWORD") ?? "";
+            var connectionString = connectionStringTemplate.Replace("{password}", connectionPassword);
+            var settings = MongoClientSettings.FromConnectionString(connectionString);
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase("country-database");
+            _countries = database.GetCollection<Country>("country");
+        }
+
+        public async Task<IList<Country>> GetCountries()
+        {
+            try
+            {
+                var allCountries = await _countries.FindAsync(country => true).ConfigureAwait(false);
+                return allCountries.ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<IList<Country>> GetCountriesWithFilter()
+        {
+            return new List<Country>();
+        }
+    }
+}
---
+++ Countries.Server/Repositories/ICountryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Countries.Server.Models;
+
+namespace Countries.Server.Repositories
+{
+    public interface ICountryRepository
+    {
+        public Task<IList<Country>> GetCountries();
+        public Task<IList<Country>> GetCountriesWithFilter();
+    }
+}
