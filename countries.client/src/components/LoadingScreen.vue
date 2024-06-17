c50117b990f71764b73657c16d2d6853809346b5
Huy Phan <huy.phan2210@outlook.com>
2024-06-17 10:57:48 +0700

    Create LoadingScreen.vue

---
+++ countries.client/src/components/LoadingScreen.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="loading-screen">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path
+        d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z"
+        class="spinner_6kVp"
+      />
+    </svg>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.loading-screen {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background-color: rgba(255, 255, 255, 0.8);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.spinner_6kVp {
+  transform-origin: center;
+  animation: spinner_irSm 0.75s infinite linear;
+}
+
+@keyframes spinner_irSm {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
