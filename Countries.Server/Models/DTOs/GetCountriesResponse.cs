47349d082999c688c2fd8e2cf06c35ab4d5c5957
Huy Phan <huy.phan2210@outlook.com>
2024-06-17 10:58:12 +0700

    Create GetCountriesResponse.cs

---
+++ Countries.Server/Models/DTOs/GetCountriesResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Countries.Server.Models.DTOs
+{
+    public class GetCountriesResponse
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Capital { get; set; } = string.Empty;
+        public int Population { get; set; }
+        public string Region { get; set; } = string.Empty;
+        public string Flag { get; set; } = string.Empty;
+    }
+}
