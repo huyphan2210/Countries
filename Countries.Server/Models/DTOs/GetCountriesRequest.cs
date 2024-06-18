d133d1941a454fc9d3f2b1dce67fa995df32a5c9
Huy Phan <huy.phan2210@outlook.com>
2024-06-18 13:27:07 +0700

    Create GetCountriesRequest.cs

---
+++ Countries.Server/Models/DTOs/GetCountriesRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Countries.Server.Models.DTOs
+{
+    public class GetCountriesRequest
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public string? SearchString { get; set; }
+        public string? Region { get; set; }
+    }
+}
